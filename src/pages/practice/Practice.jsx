5f82b792fb495068a85daabe69b1c77bd93de53a
Shivam-ACE <shivam.d@acedataanalytics.com>
2023-03-09 14:41:20 +0530

    Agents Page Done!!

--- src/pages/practice/Practice.jsx
+++ src/pages/practice/Practice.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styled from "@emotion/styled";
 import { Add, Search } from "@mui/icons-material";
 
@@ -7,16 +8,22 @@ import {
   Typography,
   Box,
   InputBase,
-  Grid,
   Button,
   TextField,
   InputAdornment,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
 } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import { alpha, spacing } from "@mui/system";
 import { DataGrid } from "@mui/x-data-grid";
 import { Helmet } from "react-helmet-async";
 import { Link, NavLink } from "react-router-dom";
+import PrimaryLogo from "../../assests/primary-logo.png";
+import SecondaryLogo from "../../assests/secondary-logo.png";
+import LifeInsuranceChip from "../../assests/life-insurance-chip.png";
+import AnnuityInsuranceChip from "../../assests/annuity-insurance-chip.png";
 
 const Divider = styled(MuiDivider)(spacing);
 
@@ -100,150 +107,435 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-// const newColumns = [
-//   { field: "name", headerName: "Name" },
-//   { field: "email", headerName: "Email" },
-//   { field: "type", headerName: "Type" },
-//   { field: "life-template", headerName: "Life Template" },
-//   { field: "annuity-template", headerName: "Annuity Template" },
-//   { field: "created-date", headerName: "Created Date" },
-//   { field: "nda-status", headerName: "NDA Status" },
+function CheckboxesGroup() {
+  const [state, setState] = React.useState({
+    signed: true,
+    not_signed: false,
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  const { signed, not_signed } = state;
+
+  return (
+    // <Card mb={6}>
+    //   <CardContent>
+    //     <Typography variant="h6" gutterBottom>
+    //       Checkboxes
+    //     </Typography>
+    //     <Typography variant="body2" gutterBottom>
+    //       Checkboxes allow the user to select one or more items from a set.
+    //     </Typography>
+    //     <Paper mt={3}>
+    //       <FormControl component="fieldset" variant="standard">
+    //         <FormLabel component="legend">Assign responsibility</FormLabel>
+    //         <FormGroup>
+    //           <FormControlLabel
+    //             control={
+    //               <Checkbox
+    //                 checked={signed}
+    //                 onChange={handleChange}
+    //                 name="signed"
+    //               />
+    //             }
+    //             label="Gilad Gray"
+    //           />
+    //           <FormControlLabel
+    //             control={
+    //               <Checkbox
+    //                 checked={not_signed}
+    //                 onChange={handleChange}
+    //                 name="not_signed"
+    //               />
+    //             }
+    //             label="Jason Killian"
+    //           />
+    //         </FormGroup>
+    //       </FormControl>
+    //     </Paper>
+    //   </CardContent>
+    // </Card>
+    <Grid2 container direction="row">
+      <Grid2 item>
+        <Typography
+          variant="outlined"
+          sx={{ color: "#7A7A7A" }}
+          gutterBottom
+          display="inline"
+        >
+          NDA status
+        </Typography>
+      </Grid2>
+      <Grid2 item>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                sx={{ padding: "0px 0px 0px 20px" }}
+                checked={signed}
+                onChange={handleChange}
+                name="signed"
+              />
+            }
+            label="Signed"
+          />
+        </FormGroup>
+      </Grid2>
+
+      <Grid2 item>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                sx={{ padding: "0px 0px 0px 20px" }}
+                checked={not_signed}
+                onChange={handleChange}
+                name="not_signed"
+              />
+            }
+            label="Not Signed"
+          />
+        </FormGroup>
+      </Grid2>
+    </Grid2>
+    // <FormControl component="fieldset" variant="standard">
+    //   <FormLabel component="legend">Assign responsibility</FormLabel>
+    //   <FormGroup>
+    //     <FormControlLabel
+    //       control={
+    //         <Checkbox checked={signed} onChange={handleChange} name="signed" />
+    //       }
+    //       label="Gilad Gray"
+    //     />
+    // <FormControlLabel
+    //   control={
+    //     <Checkbox
+    //       checked={not_signed}
+    //       onChange={handleChange}
+    //       name="not_signed"
+    //     />
+    //   }
+    //   label="Jason Killian"
+    // />
+    //   </FormGroup>
+    // </FormControl>
+  );
+}
+
+function Checkboxes() {
+  const [state, setState] = React.useState({
+    life: true,
+    annuity: false,
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  const { life, annuity } = state;
+
+  return (
+    <Grid2 container direction="row">
+      <Grid2 item>
+        <Typography
+          variant="outlined"
+          sx={{ color: "#7A7A7A" }}
+          gutterBottom
+          display="inline"
+        >
+          Type
+        </Typography>
+      </Grid2>
+      <Grid2 item>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                sx={{ padding: "0px 0px 0px 20px" }}
+                checked={life}
+                onChange={handleChange}
+                name="life"
+              />
+            }
+            label="Life"
+          />
+        </FormGroup>
+      </Grid2>
+
+      <Grid2 item>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                sx={{ padding: "0px 0px 0px 20px" }}
+                checked={annuity}
+                onChange={handleChange}
+                name="annuity"
+              />
+            }
+            label="Annuity"
+          />
+        </FormGroup>
+      </Grid2>
+    </Grid2>
+    // <FormControl component="fieldset" variant="standard">
+    //   <FormLabel component="legend">Assign responsibility</FormLabel>
+    //   <FormGroup>
+    //     <FormControlLabel
+    //       control={
+    //         <Checkbox checked={signed} onChange={handleChange} name="signed" />
+    //       }
+    //       label="Gilad Gray"
+    //     />
+    // <FormControlLabel
+    //   control={
+    //     <Checkbox
+    //       checked={not_signed}
+    //       onChange={handleChange}
+    //       name="not_signed"
+    //     />
+    //   }
+    //   label="Jason Killian"
+    // />
+    //   </FormGroup>
+    // </FormControl>
+  );
+}
+
+// const columns = [
+//   {
+//     field: "id",
+//     headerName: "ID",
+//     width: 340,
+//     align: "center",
+//     headerAlign: "center",
+//   },
+//   {
+//     field: "firstName",
+//     headerName: "First name",
+//     width: 340,
+//   },
+//   {
+//     field: "lastName",
+//     headerName: "Last name",
+//     width: 340,
+//   },
+//   {
+//     field: "age",
+//     headerName: "Age",
+//     type: "number",
+//     width: 340,
+//     align: "left",
+//     headerAlign: "left",
+//   },
 // ];
 
-const columns = [
-  {
-    field: "id",
-    headerName: "ID",
-    width: 340,
-    align: "center",
-    headerAlign: "center",
-  },
-  {
-    field: "firstName",
-    headerName: "First name",
-    width: 340,
-  },
-  {
-    field: "lastName",
-    headerName: "Last name",
-    width: 340,
-  },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 340,
-    align: "left",
-    headerAlign: "left",
-  },
-];
-
 const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35, status: "active" },
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
   {
     id: 2,
     lastName: "Lannister",
     firstName: "Cersei",
     age: 42,
-    status: "inactive",
   },
   {
     id: 3,
     lastName: "Lannister",
     firstName: "Jaime",
     age: 45,
-    status: "active",
   },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16, status: "inactive" },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
   {
     id: 5,
     lastName: "Targaryen",
     firstName: "Daenerys",
     age: 23,
-    status: "active",
   },
   {
     id: 6,
     lastName: "Melisandre",
     firstName: "ITTS",
     age: 150,
-    status: "inactive",
   },
   {
     id: 7,
     lastName: "Clifford",
     firstName: "Ferrara",
     age: 44,
-    status: "active",
   },
   {
     id: 8,
     lastName: "Frances",
     firstName: "Rossini",
     age: 36,
-    status: "inactive",
   },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65, status: "active" },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
+
+const newColumns = [
+  {
+    field: "logo",
+    headerName: "",
+    width: 50,
+    renderCell: (params) => <img src={params.value} />,
+  },
+  { field: "name", headerName: "Name", width: 190 },
+  { field: "email", headerName: "Email", width: 300 },
+  {
+    field: "type",
+    headerName: "Type",
+    width: 190,
+    renderCell: (params) => <img src={params.value} />,
+  },
+  { field: "lifeTemplate", headerName: "Life Template", width: 180 },
+  { field: "annuityTemplate", headerName: "Annuity Template", width: 180 },
+  { field: "createdDate", headerName: "Created Date", width: 170 },
+  { field: "ndaStatus", headerName: "NDA Status", width: 400 },
+];
+
+const newRows = [
+  {
+    id: 1,
+    logo: PrimaryLogo,
+    name: "Shivam",
+    email: "shivamdhyani@gmail.com",
+    type: LifeInsuranceChip,
+    lifeTemplate: "Gold",
+    annuityTemplate: "Platinum",
+    createdDate: "01/06/2023",
+    ndaStatus: "Signed",
+  },
+  {
+    id: 2,
+    logo: SecondaryLogo,
+    name: "Kaval",
+    email: "shivamdhyani@gmail.com",
+    type: AnnuityInsuranceChip,
+    lifeTemplate: "Gold",
+    annuityTemplate: "Platinum",
+    createdDate: "01/06/2023",
+    ndaStatus: "Sent at 30/12/2022 18:44:22",
+  },
+  {
+    id: 3,
+    logo: PrimaryLogo,
+    name: "Divyanshu",
+    email: "shivamdhyani@gmail.com",
+    type: LifeInsuranceChip,
+    lifeTemplate: "Gold",
+    annuityTemplate: "Platinum",
+    createdDate: "01/06/2023",
+    ndaStatus: "Signed",
+  },
+  {
+    id: 4,
+    logo: SecondaryLogo,
+    name: "Harshil",
+    email: "shivamdhyani@gmail.com",
+    type: AnnuityInsuranceChip,
+    lifeTemplate: "Gold",
+    annuityTemplate: "Platinum",
+    createdDate: "01/06/2023",
+    ndaStatus: "Sent at 30/12/2022 18:44:22",
+  },
+  {
+    id: 5,
+    logo: PrimaryLogo,
+    name: "Birud",
+    email: "shivamdhyani@gmail.com",
+    type: LifeInsuranceChip,
+    lifeTemplate: "Gold",
+    annuityTemplate: "Platinum",
+    createdDate: "01/06/2023",
+    ndaStatus: "Signed",
+  },
+  {
+    id: 6,
+    logo: SecondaryLogo,
+    name: "Nikunj",
+    email: "shivamdhyani@gmail.com",
+    type: AnnuityInsuranceChip,
+    lifeTemplate: "Gold",
+    annuityTemplate: "Platinum",
+    createdDate: "01/06/2023",
+    ndaStatus: "Sent at 30/12/2022 18:44:22",
+  },
+  {
+    id: 7,
+    logo: PrimaryLogo,
+    name: "Dhaval",
+    email: "shivamdhyani@gmail.com",
+    type: LifeInsuranceChip,
+    lifeTemplate: "Gold",
+    annuityTemplate: "Platinum",
+    createdDate: "01/06/2023",
+    ndaStatus: "Signed",
+  },
+  {
+    id: 8,
+    logo: SecondaryLogo,
+    name: "Harsh",
+    email: "shivamdhyani@gmail.com",
+    type: AnnuityInsuranceChip,
+    lifeTemplate: "Gold",
+    annuityTemplate: "Platinum",
+    createdDate: "01/06/2023",
+    ndaStatus: "Sent at 30/12/2022 18:44:22",
+  },
+  {
+    id: 9,
+    logo: PrimaryLogo,
+    name: "Mihir",
+    email: "shivamdhyani@gmail.com",
+    type: LifeInsuranceChip,
+    lifeTemplate: "Gold",
+    annuityTemplate: "Platinum",
+    createdDate: "01/06/2023",
+    ndaStatus: "Signed",
+  },
   {
     id: 10,
-    lastName: "Clifford",
-    firstName: "Ferrara",
-    age: 44,
-    status: "inactive",
+    logo: SecondaryLogo,
+    name: "Deep",
+    email: "shivamdhyani@gmail.com",
+    type: AnnuityInsuranceChip,
+    lifeTemplate: "Gold",
+    annuityTemplate: "Platinum",
+    createdDate: "01/06/2023",
+    ndaStatus: "Sent at 30/12/2022 18:44:22",
   },
-  { id: 11, lastName: "Snow", firstName: "Jon", age: 35, status: "active" },
+  {
+    id: 11,
+    logo: PrimaryLogo,
+    name: "Anand",
+    email: "shivamdhyani@gmail.com",
+    type: LifeInsuranceChip,
+    lifeTemplate: "Gold",
+    annuityTemplate: "Platinum",
+    createdDate: "01/06/2023",
+    ndaStatus: "Signed",
+  },
   {
     id: 12,
-    lastName: "Lannister",
-    firstName: "Cersei",
-    age: 42,
-    status: "inactive",
+    logo: SecondaryLogo,
+    name: "Nitin",
+    email: "shivamdhyani@gmail.com",
+    type: AnnuityInsuranceChip,
+    lifeTemplate: "Gold",
+    annuityTemplate: "Platinum",
+    createdDate: "01/06/2023",
+    ndaStatus: "Sent at 30/12/2022 18:44:22",
   },
-  {
-    id: 13,
-    lastName: "Lannister",
-    firstName: "Jaime",
-    age: 45,
-    status: "active",
-  },
-  { id: 14, lastName: "Stark", firstName: "Arya", age: 16, status: "inactive" },
-  {
-    id: 15,
-    lastName: "Targaryen",
-    firstName: "Daenerys",
-    age: 23,
-    status: "active",
-  },
-  {
-    id: 16,
-    lastName: "Melisandre",
-    firstName: "ITTS",
-    age: 150,
-    status: "inactive",
-  },
-  {
-    id: 17,
-    lastName: "Clifford",
-    firstName: "Ferrara",
-    age: 44,
-    status: "active",
-  },
-  {
-    id: 18,
-    lastName: "Frances",
-    firstName: "Rossini",
-    age: 36,
-    status: "inactive",
-  },
-  { id: 19, lastName: "Roxie", firstName: "Harvey", age: 65, status: "active" },
 ];
 
 function DataGridDemo() {
   return (
     <Box sx={{ height: 600, width: "100%" }}>
       <CustomDataGrid
-        rows={rows}
-        columns={columns}
+        rows={newRows}
+        columns={newColumns}
         pageSize={8}
         rowsPerPageOptions={[8]}
         experimentalFeatures={{ newEditingApi: true }}
@@ -260,8 +552,11 @@ const Practice = () => {
   return (
     <div>
       <Helmet title="Practice" />
-      <Grid container justifyContent="space-between">
-        <Grid item xs={8}>
+      <img src={PrimaryLogo} />
+      <img src={SecondaryLogo} />
+      {/* Navigation Section */}
+      <Grid2 container justifyContent="space-between">
+        <Grid2 item xs={4}>
           <Typography variant="h3" gutterBottom display="inline">
             Agents
           </Typography>
@@ -272,16 +567,26 @@ const Practice = () => {
             </Link>
             <Typography>Agents</Typography>
           </Breadcrumbs>
-        </Grid>
-        <Grid item xs={2.6}>
-          <Grid container justifyContent="space-between">
-            <Grid item>
+        </Grid2>
+        <Grid2 item xs={5} mt={4} direction="row">
+          <Grid2 container direction="row">
+            <Grid2 item>
+              <CheckboxesGroup />
+            </Grid2>
+            <Grid2 item>
+              <Checkboxes />
+            </Grid2>
+          </Grid2>
+        </Grid2>
+        <Grid2 item xs={2.6} mt={3}>
+          <Grid2 container justifyContent="space-between">
+            <Grid2 item>
               <Button mr={2} variant="contained" color="primary">
                 <Add />
                 New Agent
               </Button>
-            </Grid>
-            <Grid item>
+            </Grid2>
+            <Grid2 item>
               <CustomTextField
                 placeholder="Search"
                 InputProps={{
@@ -292,13 +597,14 @@ const Practice = () => {
                   ),
                 }}
               />
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
+            </Grid2>
+          </Grid2>
+        </Grid2>
+      </Grid2>
 
       <Divider my={2} mb={4} />
 
+      {/* Agent Data Table Section */}
       <Grid2 container spacing={2}>
         <Grid2 item xs={12}></Grid2>
         <Grid2 item xs={12} sx={{ backgroundColor: "#FFFFFF" }}>
